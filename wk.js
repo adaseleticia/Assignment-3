91d126b5a28b367c2a13527256e21e9e4aa1a070
adaseleticia <adaseleticia@gmail.com>
2019-06-25 14:14:52 +0900

    Delete wk.js

--- wk.js
+++
@@ -1,8 +0,0 @@
-function leti() {
-			alert('hello, I am very excited to start learning how to code.Thank DiV Team for the opportunity. Just started JS lessons');
-		}
-
-window.onload = function()
-{
-	leti();
-};
